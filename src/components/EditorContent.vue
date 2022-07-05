4249a88702ed1df486c0707052f70559ee8196d3
crazyilian <ilian.and.04@gmail.com>
2022-07-05 16:30:14 +0300

    [refactor]: grade title component

--- src/components/EditorContent.vue
+++ src/components/EditorContent.vue
@@ -5,18 +5,7 @@
   >
     <div style="display: flex; justify-content: space-between">
       <h3 class="px-2">{{ template.name }}</h3>
-      <div style="margin-right: 40px; display: flex;">
-        <div
-            v-for="(grade, i) in template.grades"
-            :key="i"
-            class="pb-3"
-            style="width: 48px; display: flex; align-items: flex-end; justify-content: center"
-            @mouseover="highlightGrade(i, true)"
-            @mouseleave="highlightGrade(i, false)"
-        >
-          <div class="grade-name">{{ grade.name }}</div>
-        </div>
-      </div>
+      <GradeTitle style="margin-right: 40px" :grades="template.grades" @highlight-grade="highlightGrade"/>
     </div>
     <Table :categories="template.categories" :grades="template.grades" :grade-highlight="gradeHighlight"/>
   </v-container>
@@ -25,11 +14,13 @@
 <script>
 
 import Table from "@/components/Table";
+import GradeTitle from "@/components/GradeTitle";
 import Vue from "vue";
 
 export default {
   name: 'EditorContent',
   components: {
+    GradeTitle,
     Table
   },
 
@@ -49,15 +40,3 @@ export default {
   }
 };
 </script>
-<style>
-
-.grade-name {
-  font-weight: bold;
-  font-size: 11pt;
-  word-wrap: break-word;
-  white-space: pre-line;
-  text-align: center;
-  width: 100%;
-}
-
-</style>
---
+++ src/components/GradeTitle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="display: flex">
+    <div
+        v-for="(grade, i) in grades"
+        :key="i"
+        class="pb-3"
+        style="width: 48px; display: flex; align-items: flex-end; justify-content: center"
+        @mouseover="highlightGrade(i, true)"
+        @mouseleave="highlightGrade(i, false)"
+    >
+      <div class="grade-name">{{ grade.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "GradeTitle",
+  props: {
+    grades: { type: Array, default: () => [] }
+  },
+  methods: {
+    highlightGrade(i, flag) {
+      this.$emit('highlight-grade', i, flag);
+    }
+  }
+}
+</script>
+
+<style>
+
+.grade-name {
+  font-weight: bold;
+  font-size: 11pt;
+  word-wrap: break-word;
+  white-space: pre-line;
+  text-align: center;
+  width: 100%;
+}
+
+</style>
+
