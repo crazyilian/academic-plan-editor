67556945a0d77ab20f8b0f9a946d5b9544b34f31
crazyilian <ilian.and.04@gmail.com>
2022-08-08 23:47:37 +0300

    [feat]: show grade profile tooltip on grade name hover

--- src/components/Grades/Grade.vue
+++ src/components/Grades/Grade.vue
@@ -1,29 +1,30 @@
 <template>
-  <div
-      style="width: 48px; height: 100%; display: flex; flex-direction: column; padding: 2px 0; justify-content: space-between"
-      @mouseover="setHighlight(true)"
-      @mouseleave="setHighlight(false)"
-  >
-    <div class="pb-0" style="min-height: 0; flex-grow: 1">
-      <v-tooltip bottom :open-delay="500">
-        <template #activator="{ on, attrs} ">
+  <v-tooltip bottom :open-delay="500">
+    <template #activator="{ on, attrs} ">
+      <div
+          v-bind="attrs"
+          style="width: 48px; height: 100%; display: flex; flex-direction: column; padding: 2px 0; justify-content: space-between"
+          v-on="on"
+          @mouseover="setHighlight(true)"
+          @mouseleave="setHighlight(false)"
+      >
+        <div class="pb-0" style="min-height: 0; flex-grow: 1">
           <div
-              v-bind="attrs" class="grade-name"
+              class="grade-name"
               style="writing-mode: vertical-rl; transform: rotate(180deg)"
-              v-on="on"
           >
             <span style="font-size: 0.8em; overflow: hidden">{{ parsed.last }}</span>
           </div>
-        </template>
-        <div class="pa-2" style="text-align: center; max-width: 200px; word-wrap: break-word">
-          {{ parsed.pretty }}
         </div>
-      </v-tooltip>
+        <div class="grade-name" style="height: 1rem">
+          <span>{{ grade.name }}</span>
+        </div>
+      </div>
+    </template>
+    <div class="pa-2" style="text-align: center; max-width: 200px; word-wrap: break-word">
+      {{ parsed.pretty }}
     </div>
-    <div class="grade-name" style="height: 1rem">
-      <span>{{ grade.name }}</span>
-    </div>
-  </div>
+  </v-tooltip>
 </template>
 
 <script>
